0135ad4e62a54c8370a24c18c57f78be86e51acf
Alex Swan <alex@bold-it.com>
2015-08-22 00:24:07 -0700

    Face Planting dino

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManagerScript : MonoBehaviour {
+
+	AudioSource beat;
+	float lastBeatTime;
+	
+	List<GameObject> globes;
+	
+	Camera mainCamera;
+	GameObject player;
+	
+	// Use this for initialization
+	void Start () {
+		globes = new List<GameObject>();
+		beat = gameObject.GetComponent<AudioSource>();
+		beat.Play();
+		lastBeatTime = beat.time;
+		mainCamera = Camera.main;
+		player = GameObject.FindGameObjectWithTag("Player");
+		
+		GameObject globe = NewObjectPoolerScript.current.Spawn("Globe");
+		globe.SetActive(true);
+		globes.Add(globe);
+		
+		player.transform.parent = globe.transform;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(beat.time < lastBeatTime){
+			// We've looped, send an event
+			UpdatePlayer ();
+		}
+		
+		
+		lastBeatTime = beat.time;
+	}
+	
+	
+	void UpdatePlayer(){
+		Debug.Log ("Updating the player");
+		// Send a ray to the origin from the camera (Ignoring the player).
+		RaycastHit hit = new RaycastHit();
+		
+//		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Vector3 cameraPosition = mainCamera.transform.position;
+		Vector3 fwd = -1.0F * cameraPosition;
+		float dist = fwd.magnitude;
+		
+		if(Physics.Raycast (cameraPosition, fwd, out hit, dist)){
+			Debug.Log ("Hit something" + hit.point);
+			Debug.DrawRay(cameraPosition, fwd, Color.red, 1.0F );
+			Transform objectHit = hit.transform;
+			
+			// If the objectHit is in the current globe
+			player.transform.position = hit.point; // hit.rigidbody.centerOfMass;
+			player.transform.LookAt(Vector3.zero);
+			Debug.Log ("Center" + hit.rigidbody.centerOfMass);
+			 
+		}
+		
+		// TODO: Raycast should not hit player
+		// If it hits a hexagon/pentagon in the current globe
+			// Player moves to tile's center of mass.
+	}
+	
+}
---
+++ Assets/Scripts/RotateOnTouch.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateOnTouch : MonoBehaviour {
+	float h;
+	float v;
+	float horizontalSpeed = 1.0F;
+	float verticalSpeed = 1.0F;
+	
+	// A setting you might want to allow access to?
+	float sensitivity = 1.0F;
+	
+	Vector3 lastMousePosition;
+	GameObject mainCamera;
+	
+	// Use this for initialization
+	void Start () {
+		lastMousePosition = Vector3.zero;
+		mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.touchCount == 1){
+			Touch touch = Input.GetTouch(0);
+			if(touch.phase == TouchPhase.Moved){
+				h = horizontalSpeed * touch.deltaPosition.x * sensitivity;
+				//				transform.Rotate (0, -h, 0, Space.World);
+				transform.RotateAround(transform.position, mainCamera.transform.up, -h);
+				
+				v = verticalSpeed * touch.deltaPosition.y * sensitivity;
+				//				transform.Rotate (v, 0, 0, Space.World);
+				transform.RotateAround (transform.position, mainCamera.transform.right, v);
+			}
+		} else if (Input.GetMouseButton(0) == true) { // Mouse control
+			if(Input.GetMouseButtonDown(0)){ lastMousePosition = Input.mousePosition;}
+			transform.Rotate(Vector3.down * (Input.mousePosition.x - lastMousePosition.x), Space.World);
+			transform.Rotate(Vector3.right * (Input.mousePosition.y - lastMousePosition.y), Space.World);
+			lastMousePosition = Input.mousePosition;
+		} 
+	}
+}
